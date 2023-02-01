6de51db1645010d7d1b0f1518d0293ad054af98b
Ashmin Jayson <ashminjayson10@gmail.com>
2023-02-02 03:24:56 +0530

    feat: Responsive navbar and Countdown timer

---
+++ script.js
@@ -0,0 +1,45 @@
+"use strict"
+
+
+// Responsive Navbar 
+
+const primaryNavigation = document.querySelector('.primary-navigation')
+const navToggle= document.querySelector('#nav-toggle')
+
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNavigation.getAttribute('data-visible')
+    
+    if (visibility === 'false') {
+        primaryNavigation.setAttribute('data-visible', 'true')
+        navToggle.setAttribute('aria-expanded', 'true')
+    }
+    else {
+        primaryNavigation.setAttribute('data-visible', 'false')
+        navToggle.setAttribute('aria-expanded', 'false')
+    }
+})
+
+const navLinks = document.querySelectorAll('.nav-link')
+navLinks.forEach((navLink) => {
+    navLink.addEventListener('click', () => {
+        primaryNavigation.setAttribute('data-visible', 'false')
+        navToggle.setAttribute('aria-expanded', 'false')
+    })
+})
+
+
+// Countdown Timer
+
+const eventDate = new Date("Jun 18, 2023 08:00:00").getTime()
+
+const countDown = setInterval(() => {
+    const currentDate = new Date().getTime()
+   
+    const timeToEvent = eventDate - currentDate
+    const days = Math.floor(timeToEvent / (1000 * 60 * 60 * 24));   
+    const hours = Math.floor((timeToEvent % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeToEvent % (1000 * 60 * 60)) / (1000 * 60)); 
+    const seconds = Math.floor((timeToEvent % (1000 * 60)) / 1000);
+
+    
+}, 1000);
